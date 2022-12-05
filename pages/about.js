e1897eb212784cc0d43c4d2bbafb4f346939e877
Roba Nath Basnet <robanbth@gmail.com>
2022-12-05 11:46:01 +0600

    Nextjs Head component added to include title and the metadata of the page for the SEO

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,16 @@
+import Head from "next/head";
+
 function AboutPage() {
 	return (
-		<main>
-			<h1>About</h1>
-		</main>
+		<>
+			<Head>
+				<title>About</title>
+				<meta name="description" value="This is my about page" />
+			</Head>
+			<main>
+				<h1>About</h1>
+			</main>
+		</>
 	);
 }
 export default AboutPage;
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
+import Head from "next/head";
 function HomePage() {
 	return (
 		<>
+			<Head>
+				<title>My Blog</title>
+				<meta name="description" value="This is my blog" />
+			</Head>
 			<main>
 				<h1>My Blog</h1>
 			</main>
