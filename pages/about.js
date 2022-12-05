19172a04b23f66862732d54721d7e3423ab112d1
Roba Nath Basnet <robanbth@gmail.com>
2022-12-05 11:08:33 +0600

    Pages and basic navigation added

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+function AboutPage() {
+	console.log("AboutPage Rendered");
+	return (
+		<main>
+			<h1>About</h1>
+		</main>
+	);
+}
+export default AboutPage;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,22 @@
 function HomePage() {
+	console.log("HOme page Rendered");
 	return (
-		<main>
-			<h1>My Blog</h1>
-		</main>
+		<>
+			<header>
+				<nav>
+					<ul>
+						<li>
+							<a href="/about">About</a>
+						</li>
+						{/* <li></li>
+						<li></li> */}
+					</ul>
+				</nav>
+			</header>
+			<main>
+				<h1>My Blog</h1>
+			</main>
+		</>
 	);
 }
 export default HomePage;
