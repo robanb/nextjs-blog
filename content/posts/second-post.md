b492face0bcdf2068c0504eda20a9f80cd5c9c04
Roba Nath Basnet <robanbth@gmail.com>
2022-12-06 13:28:50 +0600

    added dynamic Routes and getStaticPaths

---
+++ content/posts/second-post.md
@@ -0,0 +1,6 @@
+---
+date: "2021-04-29"
+title: "Second Post"
+---
+
+This my Second post, written using markdown!
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,10 +1,21 @@
 import Head from "next/head";
 import { getPost } from "../../lib/posts";
 
+// adding dynamic path to different pages
+export async function getStaticPaths() {
+	return {
+		paths: [
+			{ params: { slug: "first-post" } },
+			{ params: { slug: "second-post" } },
+		],
+		fallback: false,
+	};
+}
+
 // Executed only in server side
-export async function getStaticProps() {
-	console.log("[FirstPostPage] getStaticProps()");
-	const post = await getPost("first-post");
+export async function getStaticProps({ params: { slug } }) {
+	console.log("[PostPage] getStaticProps(): ", slug);
+	const post = await getPost(slug);
 
 	return {
 		props: {
@@ -15,7 +26,7 @@ export async function getStaticProps() {
 
 // Executed in client side
 function PostPage({ post }) {
-	console.log("[FirstPostPage] render: ", post);
+	console.log("[PostPage] render: ", post);
 	return (
 		<>
 			<Head>
